ebf0cf511d8eaf36b298e7bb32eb3624270fa243
Peter Lejeck <peter.lejeck@gmail.com>
2014-04-14 15:11:24 -0700

    Add jekyll task to Grunt for testing purposes~

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,10 +3,20 @@ module.exports = function (grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 		watch: {
 			scripts: {
-				files: ['css/*.less'],
-				tasks: ['less:development'],
+				files: ['**/*'],
+				tasks: ['less:development', 'jekyll:development'],
 				options: {
 					spawn: false,
+					livereload: true
+				}
+			}
+		},
+		jekyll: {
+			development: {
+				options: {
+					dest: '/home/plejeck/public_html/stormbit-jekyll',
+					config: '_config.yml,_config-dev.yml',
+					safe: true
 				}
 			}
 		},
@@ -37,8 +47,9 @@ module.exports = function (grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-jekyll');
 
 	grunt.registerTask('production', ['less:production']);
-	grunt.registerTask('development', ['less:development']);
+	grunt.registerTask('development', ['less:development', 'jekyll:development']);
 	grunt.registerTask('default', ['less:development']);
 };
